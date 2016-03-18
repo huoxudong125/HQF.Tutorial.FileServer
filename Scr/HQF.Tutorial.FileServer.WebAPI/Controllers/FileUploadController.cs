6f4878af6ab0a258385e53b6517a9c6121b62f1d
huoxudong125 <423huo@163.com>
2016-03-18 20:36:39 +0800

    init Solution

---
+++ Scr/HQF.Tutorial.FileServer.WebAPI/Controllers/FileUploadController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using HQF.Tutorial.FileServer.WebAPI.Infrastructure;
+using HQF.Tutorial.FileServer.WebAPI.Models;
+
+namespace HQF.Tutorial.FileServer.WebAPI.Controllers
+{
+    public class FileUploadController : ApiController
+    {
+        private readonly string _uploadFolder = "~/UploadFiles";
+
+        /// <summary>
+        /// 上传文件
+        /// </summary>
+        /// <returns></returns>
+        [MimeMultipart]
+        [HttpPost]
+        public async Task<FileUploadResult> Post()
+        {
+            var uploadPath = HttpContext.Current.Server.MapPath(_uploadFolder);
+
+            var multipartFormDataStreamProvider = new UploadMultipartFormProvider(uploadPath);
+
+            // Read the MIME multipart asynchronously 
+            await Request.Content.ReadAsMultipartAsync(multipartFormDataStreamProvider);
+
+            string _localFileName = multipartFormDataStreamProvider
+                .FileData.Select(multiPartData => multiPartData.LocalFileName).FirstOrDefault();
+
+            // Create response
+            return new FileUploadResult
+            {
+                LocalFilePath = _localFileName,
+
+                FileName = Path.GetFileName(_localFileName),
+
+                FileLength = new FileInfo(_localFileName).Length
+            };
+        }
+    }
+}
---
+++ Scr/HQF.Tutorial.FileServer.WebAPI/Startup.cs
@@ -0,0 +1,36 @@
+﻿using System.Web.Http;
+using Microsoft.Owin;
+using Microsoft.Owin.Extensions;
+using Microsoft.Owin.FileSystems;
+using Microsoft.Owin.StaticFiles;
+using Owin;
+
+[assembly: OwinStartup(typeof(HQF.Tutorial.FileServer.WebAPI.Startup))]
+
+namespace HQF.Tutorial.FileServer.WebAPI
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var httpConfiguration = new HttpConfiguration();
+
+            // Configure Web API Routes:
+            // - Enable Attribute Mapping
+            // - Enable Default routes at /api.
+            WebApiConfig.Register(httpConfiguration);
+
+            app.UseWebApi(httpConfiguration);
+
+            // Make ./public the default root of the static files in our Web Application.
+            app.UseFileServer(new FileServerOptions
+            {
+                RequestPath = new PathString(string.Empty),
+                FileSystem = new PhysicalFileSystem("./public"),
+                EnableDirectoryBrowsing = true,
+            });
+
+            app.UseStageMarker(PipelineStage.MapHandler);
+        }
+    }
+}
